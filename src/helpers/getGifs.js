bc853c91bd41f537398d5625e767322ebbdef23a
melizza <chapid9@gmail.com>
2023-06-21 16:55:02 -0500

    Se cambió el límite a 11

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=Wc7VEXPda57h2OkJt6qPGLyXJXRWXdid&q=${ category }&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=Wc7VEXPda57h2OkJt6qPGLyXJXRWXdid&q=${ category }&limit=11`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
